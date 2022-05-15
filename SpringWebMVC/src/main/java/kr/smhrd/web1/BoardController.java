fb661e0ad3b34e9bf4ee23dddab2706fbcfecafd
otatai <otatai@LAPTOP-S73IEJAK>
2022-05-15 23:59:09 +0900

    ddd

---
+++ SpringWebMVC/src/main/java/kr/smhrd/web1/BoardController.java
@@ -0,0 +1,15 @@
+package kr.smhrd.web1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+	
+	@RequestMapping("/boardList.do")
+	public String boardList() {
+		
+		return "boardList";
+	}
+
+}
